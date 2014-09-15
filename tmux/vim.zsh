2d54aa70701abc676cca24a73ad699f11e1dcc65
Jose Antonio Guerra <jaguerra@icti.es>
2014-09-15 12:05:49 +0200

    [TASK] Enhance tmux

--- tmux/vim.zsh
+++ tmux/vim.zsh
@@ -1,2 +1,9 @@
 alias vim='reattach-to-user-namespace vim'
 alias mvim='reattach-to-user-namespace mvim'
+
+alias tml="tmux list-sessions"
+alias tma="tmux -2 attach -t $1"
+alias tmk="tmux kill-session -t $1"
+
+# Window names are not displaying properly?
+export DISABLE_AUTO_TITLE=true
