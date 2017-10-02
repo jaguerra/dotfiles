90e6b92e428dcc8f0f0915a04a496a1a49d40b37
Jose Antonio Guerra <jaguerra@tuenti.com>
2017-10-02 15:38:35 +0200

    Latest stuff

---
+++ java/aliases.zsh
@@ -0,0 +1,2 @@
+alias j8='export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_102.jdk/Contents/Home'
+alias j7='export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.7.0_80.jdk/Contents/Home'
