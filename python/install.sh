15ab1fffcbfab008d60a0b186bb3c7d81e7f0cc2
Jose Antonio Guerra <jaguerra@tuenti.com>
2020-10-15 15:43:17 +0200

    Add support for Python Poetry

--- python/install.sh
+++ python/install.sh
@@ -1,1 +1,9 @@
-easy_install SpoofMAC
+#!/bin/sh
+
+# Install Poetry - https://python-poetry.org/
+if test ! $(which poetry)
+then
+  echo "  Installing poetry for you."
+  curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
+fi
+
